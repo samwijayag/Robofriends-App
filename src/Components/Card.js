7d159e54367303fb6763302959bf6bd734dae506
samwijayag <sam.wiajayg@gmail.com>
2019-01-04 17:21:37 -0800

    build done, more can be added

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
 	const {name,id,email} = props
 	return (
 		<div className='bg-light-pink dib br3 pa3 ma2 grow bw2 shadow-5 tc'>
-			<img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+			<img alt='robots' src={`https://robohash.org/${id}?200x200`} />
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
